9d224df6380b518a0e22c9d23cf31449d11bcd44
Manas <88604968+manaspros@users.noreply.github.com>
2025-03-13 11:24:43 +0530

    updated img in event

--- src/components/Home/Events/events.jsx
+++ src/components/Home/Events/events.jsx
@@ -2,16 +2,20 @@ import React, { useState } from "react";
 import "./events.css";
 import EventCard from "./eventcard";
 import image1 from "../../../assets/Images/Gallery/img3.png";
+import image2 from "../../../assets/Images/Gallery/img2.png";
+import image3 from "../../../assets/Images/Gallery/img4.png";
+import image4 from "../../../assets/Images/Gallery/img5.png";
+import image5 from "../../../assets/Images/Gallery/img6.png";
 import event_image from "../../../assets/Images/Bg/eventsText.png";
 import EventModal from "./modal.jsx";
 import ContactUs from "../../ContactUs/ContactUs.jsx";
 export default function Events() {
   const [isOpen, setIsOpen] = useState(false);
   const [modalTitle, setModalTitle] = useState("");
-  const [modalText , setModalText] = useState("");
+  const [modalText, setModalText] = useState("");
 
   // Open modal with the event title
-  const openModal = (title , modal_text) => {
+  const openModal = (title, modal_text) => {
 
     setModalTitle(title);
     setModalText(modal_text);
@@ -24,29 +28,29 @@ export default function Events() {
   };
 
   return (
-    
+
     <div className="top-main">
       <div className="main-center">
         <div className="first-row">
           {!isOpen && <EventCard image_link={image1} title_text="Pro Night" onClick={() => openModal("Pro Night")} />}
-          {!isOpen &&<EventCard image_link={image1} title_text="Technical" onClick={() => openModal("Technical" )} />}
+          {!isOpen && <EventCard image_link={image2} title_text="Technical" onClick={() => openModal("Technical")} />}
         </div>
         <div className="second-row">
-          {!isOpen &&<EventCard image_link={image1} title_text="Sports" onClick={() => openModal("Sports" )} />}
-          {!isOpen &&<EventCard image_link={image1} title_text="Cultural" onClick={() => openModal("Cultural")} />}
-          {!isOpen &&<EventCard image_link={image1} title_text="e-Sports" onClick={() => openModal("e-Sports")} />}
+          {!isOpen && <EventCard image_link={image3} title_text="Sports" onClick={() => openModal("Sports")} />}
+          {!isOpen && <EventCard image_link={image4} title_text="Cultural" onClick={() => openModal("Cultural")} />}
+          {!isOpen && <EventCard image_link={image5} title_text="e-Sports" onClick={() => openModal("e-Sports")} />}
         </div>
       </div>
       <img src={event_image} id="event_text" alt="Event Text" />
 
-    
+
       {isOpen && (
         <EventModal title={modalTitle} modal_text={modalText} setIsOpen={setIsOpen} />
       )};
-      
+
     </div>
 
-  
+
 
   );
 }
