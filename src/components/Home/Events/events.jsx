86058ef61dbfa36ea054f119d940c6d99eeba8e2
Manas <88604968+manaspros@users.noreply.github.com>
2025-03-13 11:26:16 +0530

    back to sameimg

--- src/components/Home/Events/events.jsx
+++ src/components/Home/Events/events.jsx
@@ -2,10 +2,6 @@ import React, { useState } from "react";
 import "./events.css";
 import EventCard from "./eventcard";
 import image1 from "../../../assets/Images/Gallery/img3.png";
-import image2 from "../../../assets/Images/Gallery/img2.png";
-import image3 from "../../../assets/Images/Gallery/img4.png";
-import image4 from "../../../assets/Images/Gallery/img5.png";
-import image5 from "../../../assets/Images/Gallery/img6.png";
 import event_image from "../../../assets/Images/Bg/eventsText.png";
 import EventModal from "./modal.jsx";
 import ContactUs from "../../ContactUs/ContactUs.jsx";
@@ -33,12 +29,12 @@ export default function Events() {
       <div className="main-center">
         <div className="first-row">
           {!isOpen && <EventCard image_link={image1} title_text="Pro Night" onClick={() => openModal("Pro Night")} />}
-          {!isOpen && <EventCard image_link={image2} title_text="Technical" onClick={() => openModal("Technical")} />}
+          {!isOpen && <EventCard image_link={image1} title_text="Technical" onClick={() => openModal("Technical")} />}
         </div>
         <div className="second-row">
-          {!isOpen && <EventCard image_link={image3} title_text="Sports" onClick={() => openModal("Sports")} />}
-          {!isOpen && <EventCard image_link={image4} title_text="Cultural" onClick={() => openModal("Cultural")} />}
-          {!isOpen && <EventCard image_link={image5} title_text="e-Sports" onClick={() => openModal("e-Sports")} />}
+          {!isOpen && <EventCard image_link={image1} title_text="Sports" onClick={() => openModal("Sports")} />}
+          {!isOpen && <EventCard image_link={image1} title_text="Cultural" onClick={() => openModal("Cultural")} />}
+          {!isOpen && <EventCard image_link={image1} title_text="e-Sports" onClick={() => openModal("e-Sports")} />}
         </div>
       </div>
       <img src={event_image} id="event_text" alt="Event Text" />
